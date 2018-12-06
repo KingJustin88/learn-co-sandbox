642628659cdb5c97a009657524f0f09e963a6304
Justin Hirata <jlhirata88@aol.com>
2018-12-06 00:49:38 +0000

    Automatically backed up by Learn

---
+++ prac.js
@@ -0,0 +1,14 @@
+var myScope = outerFunction
+myScope;
+
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+myScope();
+
